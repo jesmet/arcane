7ff5a62738c511bd0580b78baee6ee6824ad1201
jesmet <jesmetsunny9@gmail.com>
2024-11-22 22:06:31 +0530

    Create For loop.py

---
+++ For loop.py
@@ -0,0 +1,9 @@
+
+#Prompt the user to input a number N.
+
+
+#Use a for loop to print the multiplication table for N from 1 to 10.
+#Format the output as:
+#N x 1 = result
+#N x 2 = result
+#And so on, up to N x 10 
