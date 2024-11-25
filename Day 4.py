95a011e350a5393ddaa5b5973136d9b9e987d2e2
jesmet <jesmetsunny9@gmail.com>
2024-11-25 21:00:33 +0530

    Create Day 4.py

---
+++ Day 4.py
@@ -0,0 +1,17 @@
+#Day 4 
+##while loop
+#Write a Python program to simulate a "Number Guessing Game".
+
+#1. The program should generate a random number between 1 and 100 (inclusive).
+
+
+#2. The user has to guess the number.
+
+
+#3. After each guess, the program will give feedback:
+
+#If the guess is too high, the program should print "Too high! Try again."
+
+#If the guess is too low, the program should print "Too low! Try again."
+
+#If the guess is correct, the program should print "Correct! You guessed the number."
