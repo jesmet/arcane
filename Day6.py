b8f8eece67fd949b2ce94298b569325fb6bf7cda
jesmet <jesmetsunny9@gmail.com>
2024-11-27 22:20:01 +0530

    Create Day6.py

---
+++ Day6.py
@@ -0,0 +1,24 @@
+#Day 6
+#list
+#1. Create a list called vegetables with the elements: "Carrot", "Potato", "Tomato".
+
+
+#2. Create another list called fruits with the elements: "Apple", "Banana", "Cherry".
+
+
+#3. Combine the two lists into a new list called grocery.
+
+
+#4. Print the grocery list.
+
+
+#5. Sort the grocery list in alphabetical order using the appropriate list function.
+
+
+#6. Print the sorted grocery list.
+
+
+
+
+---
+
