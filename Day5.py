15e40c9fd3d4fa3377903f1cfa536fd20596ec44
jesmet <jesmetsunny9@gmail.com>
2024-11-27 22:13:38 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,20 @@
+#Day 5 
+
+#Question:
+
+#1. Create a list called place with the following elements: "Udaipur", "Jaipur", "Mumbai", "Kerala", and "Goa".
+
+
+#2. Print the list.
+
+
+#3. Add "Kochi" to the list using the appropriate list function.
+
+
+#4. Remove "Goa" from the list using the appropriate list function.
+
+
+#5. Print the updated list after each modification.
+
+
+
