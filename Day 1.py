47e862f1532b335a28c19147f704246e504fc4eb
jesmet <jesmetsunny9@gmail.com>
2024-12-12 20:31:22 +0530

    Create Day 1.py

---
+++ Day 1.py
@@ -0,0 +1,8 @@
+#Write a Python program to check if a driver is overspeeding.
+
+#The program should 
+
+#1. Prompt the user to enter the speed of the car (in km/h)
+#2. If the speed is greater than 60, display the message: "You are overspeeding! Slow down!"
+#3. If the speed is 60 or less, display nothing
+
