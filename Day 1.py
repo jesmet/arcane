96cf0a035c3f2c7c24652b5a63860bcb77d7acbb
jesmet <jesmetsunny9@gmail.com>
2024-12-12 20:39:44 +0530

    Update Day 1.py

--- Day 1.py
+++ Day 1.py
@@ -6,3 +6,24 @@
 #2. If the speed is greater than 60, display the message: "You are overspeeding! Slow down!"
 #3. If the speed is 60 or less, display nothing
 
+
+
+
+
+#hashim
+
+S=int(input("Enter the speed of the vehicle"))
+if S>60:
+   print("you are overspeeding !slow down!")
+
+
+#farsana
+speed=int(input("enter the speed of the car (in km/h)"))
+if speed >=60:
+    print("you are overspeeding! slow down!")
+
+#faris
+speed = int(input("enter the speed ")
+if speed > 60:
+   print("you are overspeeding ! slow down!")
+            
